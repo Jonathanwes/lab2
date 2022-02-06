f4398afd1344a05be0580b1bbae5dfaf918ad732
isaklagerberg <98961202+isaklagerberg@users.noreply.github.com>
2022-02-06 19:46:40 +0100

    Updaterad verision

--- lab2/Main.java
+++ lab2/Main.java
@@ -1,12 +1,10 @@
+
 package lab2;
 
 import lab2.Driver;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Driver driver1 = new Driver();
-
-        driver1.run();
-    }
+	public static void main(String[] args) {
+		
+	}
 }
