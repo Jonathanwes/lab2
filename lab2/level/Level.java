2678b004dd6cdc077db6da221763c0f631e1f440
Jonathanwes <75334370+Jonathanwes@users.noreply.github.com>
2022-02-07 11:33:51 +0100

    Update Level.java
    Lägger till playerRoom som håller koll på vilket rum spelaren är i.
    
    Lägger till en metod MoveDirection som ser om det finns en koppling från playerRoom i riktningen som spelaren har försökt gå till.
    Finns det en koppling går spelaren åt det hållet med hjälp av metoden changeRoom
    
    Lägger till en metod ChangeRoom som ändrar playeroom till rummet som är kopplat åt hållet spelaren försöker röra på sig. dvs som har en doorway åt det hållet
    
    Koden fungerar säkert inte eftersom jag har skrivit i github hehe :)

--- lab2/level/Level.java
+++ lab2/level/Level.java
@@ -8,6 +8,7 @@ public class Level extends Observable {
     public ArrayList<Room> rooms = new ArrayList<>(); /*  Arraylist is a dynamic sized data structure which doesn't require a specific size at the time of initialization. */
     int playerX;
     int playerY;
+    int playerRoom;
 
     public boolean place(Room r, int x, int y)  {
 
@@ -34,6 +35,35 @@ public class Level extends Observable {
     public void firstLocation(Room r) {
         playerX = rooms.get(0).dx/2;
         playerY = rooms.get(0).dy/2;
+        playerRoom=rooms.get(0); // för att lättare kunna hålla koll på rummet spelaren är i och vilka kopplingar rummet har
     }
-
+    public void moveDirection(str Direction) { // den här ska kallas från listner där tangenten är ett input den testar om det går att förflytta spelaren åt ett håll
+        connections = playerRoom.connections;
+        for (connections : connections){
+            if connections == Direction;
+                this.ChangeRoom(Direction) // gör att spelaren förflyttar rum
+                return true
+         return false
+    }
+    public void ChangeRoom(Direction){// förflyttar spelaren till ett anant rum
+        switch(Direction){
+        case "North": 
+            playerRoom=playerRoom.northernDoorway; // om det finns en koppling är rum.[riktning]Doorway rummet i den riktningen
+            Break;
+        case "East": 
+            playerRoom=playerRoom.easternDoorway;
+            Break;
+        case "South": 
+            playerRoom=playerRoom.southernDoorway;
+            Break;
+        case "West": 
+            playerRoom=playerRoom.westernDoorway;
+            Break;
+        default: 
+            return // eftersom spelaren inte byter plats om det inte går att flytta sig i riktningen.
+        }
+        playerX=playerRoom.x+playerRoom.dx/2 // uppdaterar spelaren till mitten av det nya rummet
+        playerY=playerRoom.y+playerRoom.dy/2
+        
+    }
 }
